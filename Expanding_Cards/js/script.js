0d29c57cb3b483fd332291a5d3eb8e490eee9290
Renan Rosales <alfredorg9306@gmail.com>
2024-05-07 10:07:46 -0600

    Ocultar y desocultar imagenes

---
+++ Expanding_Cards/js/script.js
@@ -0,0 +1,17 @@
+const panels = document.querySelectorAll('.panel');
+console.log(panels);
+
+panels.forEach((panel)=>{
+    panel.addEventListener('click', ()=>{
+        removeClass();
+        panel.classList.add('active')
+
+
+    })
+})
+
+function removeClass(){
+    panels.forEach((panel)=>{
+        panel.classList.remove('active');
+    })
+}
